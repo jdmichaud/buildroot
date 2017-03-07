ee8e8357a90ceca1222ad5bd4e7aa10f252f4e8d
Damien Riegel <damien.riegel@savoirfairelinux.com>
2017-03-06 19:56:19 -0500

    raspberrypi3: add dropbear

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -9,3 +9,12 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+# Dropbear files
+mkdir -p ${TARGET_DIR}/etc/default/
+echo DROPBEAR_ARGS=-B >${TARGET_DIR}/etc/default/dropbear
+if [ -L "${TARGET_DIR}/etc/dropbear" ]; then
+	rm ${TARGET_DIR}/etc/dropbear
+	mkdir -p ${TARGET_DIR}/etc/dropbear/
+fi
+cp ${0%/*}/dropbear_ecdsa_host_key ${TARGET_DIR}/etc/dropbear/
